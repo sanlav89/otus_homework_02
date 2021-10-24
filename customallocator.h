8630310465b26e92e45ebf5b95560785e88d22c9
sanlav89 <sanlav89@mail.ru>
2021-10-24 18:23:50 +0300

    implement constructor amd dectructor for CustomAllocator

--- customallocator.h
+++ customallocator.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <iostream>
+#include <list>
 
 template <typename T>
 class CustomAllocator
@@ -14,55 +15,73 @@ public:
     using const_pointer = const T*;
     using reference = T&;
     using const_reference = const T&;
+    static constexpr int BlockSize = 10;
 
-    CustomAllocator() = default;
-    ~CustomAllocator() = default;
+    template<typename U>
+    struct rebind {
+        using other = CustomAllocator<U>;
+    };
 
-//    CustomAllocator(const int &part_size)
+    CustomAllocator()
+        : m_blockSize(BlockSize)
+        , m_cntOfFreeBlocks(0)
+        , m_p(nullptr)
+    {
+        m_p = reinterpret_cast<T *>(std::malloc(m_blockSize * sizeof(T)));
+        if (m_p == nullptr) {
+            throw std::bad_alloc();
+        }
+    }
+
+    ~CustomAllocator()
+    {
+        if (m_p != nullptr) {
+            std::free(m_p);
+        }
+    }
 
     template <typename U>
-//    CustomAllocator(const CustomAllocator<U>&);
-    CustomAllocator(const CustomAllocator<U>&)
+    CustomAllocator(const CustomAllocator<U> &)
     {
         // should we make a copy of the rhs.m_buffer ?
         // No, we should not!
     }
 
-//    T* allocate (std::size_t n);
     T* allocate (std::size_t n)
     {
         std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" << std::endl;
-        auto p = std::malloc(n * sizeof(T));
-        if (!p) {
+        if (m_blockSize - m_cntOfFreeBlocks >= n) {
+            T *p = &m_p[m_cntOfFreeBlocks];
+            m_cntOfFreeBlocks += n;
+            return p;
+        } else {
             throw std::bad_alloc();
         }
-        return reinterpret_cast<T *>(p);
+
     }
 
-//    void deallocate (T* p, std::size_t n);
-    void deallocate (T* p, std::size_t n)
+    void deallocate (T* , std::size_t n)
     {
         std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" << std::endl;
-        std::free(p);
     }
 
     template<typename U, typename ...Args>
-//    void construct(U *p, Args &&...args);
     void construct(U *p, Args &&...args)
     {
         std::cout << __PRETTY_FUNCTION__ << std::endl;
         new(p) U(std::forward<Args>(args)...);
     }
 
-//    void destroy(T *p);
     void destroy(T *p)
     {
         std::cout << __PRETTY_FUNCTION__ << std::endl;
         p->~T();
     }
 
-//private:
-//    int m_size;
+private:
+    std::size_t m_blockSize;
+    std::size_t m_cntOfFreeBlocks;
+    T *m_p;
 
 };
 
--- main.cpp
+++ main.cpp
@@ -13,37 +13,24 @@ int main()
     utils::fill_map_by_factorial(map_std, map_size);
     utils::print_map(map_std);
 
-    std::cout << "\n\n\n\n****  next part ********\n\n\n\n" << std::endl;
-
-//    auto tmp = std::initializer_list<std::pair<const int, int>>{
-//        {0, 0},
-//        {1, 0},
-//        {2, 0},
-//        {3, 0},
-//        {4, 0},
-//        {5, 0},
-//        {6, 0},
-//        {7, 0},
-//        {8, 0},
-//        {9, 0}
-//    };
-
+//    std::cout << "\n\n\n\n****  next part ********\n\n\n\n" << std::endl;
 
     auto map_custom_allocator = utils::map_custom_alloc<int, int>{};
     utils::fill_map_by_factorial(map_custom_allocator, map_size);
     utils::print_map(map_custom_allocator);
+//    int *a = static_cast<int *>(&map_custom_allocator.begin());
 
-    std::cout << "\n\n\n\n****  next part ********\n\n\n\n" << std::endl;
+//    std::cout << "\n\n\n\n****  next part ********\n\n\n\n" << std::endl;
 
 //    std::vector<int> vector_std;
-    std::vector<int, CustomAllocator<int>> vector;
-    for (int i = 0; i < map_size; ++i) {
-        vector.push_back(i);
-    }
-    for (const auto& v : vector) {
-        std::cout << v << ' ';
-    }
-    std::cout << std::endl;
+//    std::vector<int, CustomAllocator<int>> vector;
+//    for (int i = 0; i < map_size; ++i) {
+//        vector.push_back(i);
+//    }
+//    for (const auto& v : vector) {
+//        std::cout << v << ' ';
+//    }
+//    std::cout << std::endl;
 
 //    std::cout << "\n\n\n\n****  next part ********\n\n\n\n" << std::endl;
 
