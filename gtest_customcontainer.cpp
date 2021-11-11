6a1b82b05460dbabe915bb775df702a55fc30fb7
sanlav89 <sanlav89@mail.ru>
2021-11-11 09:26:51 +0300

    Revert "remove files for review"
    This reverts commit e70aa6956834a26b30c3a485c7a9273cef63bf19.

---
+++ gtest_customcontainer.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+
+#include "customcontainer.h"
+
+TEST(CustomContainer, BasicAssertions)
+{
+    CustomContainer<int> v(10);
+    v.push_back(5);
+    v.push_back(6);
+    v.push_back(7);
+    v.push_back(8);
+    v.push_back(9);
+    v.push_back(10);
+    v.push_back(11);
+    v.push_back(12);
+
+    EXPECT_EQ(v[2], 7);
+    EXPECT_EQ(v.size(), 8);
+
+    CustomContainer<int>::iterator it = v.begin();
+    EXPECT_EQ(*it, 5);
+    it++;
+    EXPECT_EQ(*it, 6);
+
+    v.push_back(13);
+    it = v.end();
+    v.push_back(14);
+
+    EXPECT_EQ(*it, 14);
+}
---
+++ gtest_sample.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions)
+{
+    EXPECT_TRUE(1);
+}
+
